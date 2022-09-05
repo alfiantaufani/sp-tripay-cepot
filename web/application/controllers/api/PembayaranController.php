aeceb3713568c003074129f56d2281b11b2b1552
alfiantaufani <alfiantaufani@gmail.com>
2022-09-06 01:35:50 +0700

    tes

--- web/application/controllers/api/PembayaranController.php
+++ web/application/controllers/api/PembayaranController.php
@@ -117,12 +117,12 @@ class PembayaranController extends CI_Controller
         $merchantRef = 'DONASI-CEPOT-' . (int)preg_replace('/(0)\.(\d+) (\d+)/', '$3$1$2', microtime());
         $init = $this->tripay->initTransaction($merchantRef);
 
-        $signature = $init->createSignature();
+        // $signature = $init->createSignature();
 
         // $init->setMethod('BRIVAOP');
         $init->setMethod($this->input->get('method'));
-        $transaction = $init->openTransaction();
 
+        $transaction = $init->openTransaction();
         $transaction->setPayload([
             'method'            => $init->getMethod(),
             'merchant_ref'      => $merchantRef,
@@ -130,49 +130,49 @@ class PembayaranController extends CI_Controller
             'signature'         => $init->createSignature()
         ]);
 
-        // return $this->output->set_content_type('application/json')
-        //     ->set_status_header(200)
-        //     ->set_output(json_encode($transaction->getData()));
-        $result = $transaction->getData();
+        return $this->output->set_content_type('application/json')
+            ->set_status_header(200)
+            ->set_output(json_encode($transaction->getData()));
+        // $result = $transaction->getData();
 
-        $transaksi = [
-            'uuid' => $result->uuid,
-            'merchant_ref' => $result->merchant_ref,
-            'customer_name' => $result->customer_name,
-            'payment_name' => $result->payment_name,
-            'payment_method' => $result->payment_method,
-            'pay_code' => $result->pay_code,
-            'qr_string' => $result->qr_string,
-            'qr_url' => $result->qr_url,
-            'status' => 'PENDING',
-        ];
-        $insert = $this->db->insert('transaksi', $transaksi);
-        $id_pembayaran = $this->db->insert_id();
+        // $transaksi = [
+        //     'uuid' => $result->uuid,
+        //     'merchant_ref' => $result->merchant_ref,
+        //     'customer_name' => $result->customer_name,
+        //     'payment_name' => $result->payment_name,
+        //     'payment_method' => $result->payment_method,
+        //     'pay_code' => $result->pay_code,
+        //     'qr_string' => $result->qr_string,
+        //     'qr_url' => $result->qr_url,
+        //     'status' => 'PENDING',
+        // ];
+        // $insert = $this->db->insert('transaksi', $transaksi);
+        // $id_pembayaran = $this->db->insert_id();
 
-        $detail_transaksi = [
-            'id_pembayaran' => $id_pembayaran,
-            'deskripsi' => $this->input->get('nama'),
-            'kode_campaign' => $this->input->get('kode'),
-            'nominal' => $this->input->get('nominal'),
-        ];
-        $this->db->insert('detail_transaksi', $detail_transaksi);
+        // $detail_transaksi = [
+        //     'id_pembayaran' => $id_pembayaran,
+        //     'deskripsi' => $this->input->get('nama'),
+        //     'kode_campaign' => $this->input->get('kode'),
+        //     'nominal' => $this->input->get('nominal'),
+        // ];
+        // $this->db->insert('detail_transaksi', $detail_transaksi);
 
-        if ($insert) {
-            $this->db->delete('keranjang', array('id' => $this->input->get('idkeranjang')));
-            return $this->output->set_content_type('application/json')
-                ->set_status_header(200)
-                ->set_output(json_encode([
-                    'status' => 'success',
-                    'message' => 'Pembayaran berhasil, silahkan klick ok',
-                ]));
-        } else {
-            return $this->output->set_content_type('application/json')
-                ->set_status_header(500)
-                ->set_output(json_encode([
-                    'status' => 'error',
-                    'message' => 'Pembayaran gagal'
-                ]));
-        }
+        // if ($insert) {
+        //     $this->db->delete('keranjang', array('id' => $this->input->get('idkeranjang')));
+        //     return $this->output->set_content_type('application/json')
+        //         ->set_status_header(200)
+        //         ->set_output(json_encode([
+        //             'status' => 'success',
+        //             'message' => 'Pembayaran berhasil, silahkan klick ok',
+        //         ]));
+        // } else {
+        //     return $this->output->set_content_type('application/json')
+        //         ->set_status_header(500)
+        //         ->set_output(json_encode([
+        //             'status' => 'error',
+        //             'message' => 'Pembayaran gagal'
+        //         ]));
+        // }
     }
 
     public function callback()
