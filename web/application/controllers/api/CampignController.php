1c7b25c7d39a82e02fa809d24d17bb3313bb68d9
alfiantaufani <alfiantaufani@gmail.com>
2022-09-06 15:23:18 +0700

    chanel

--- web/application/controllers/api/CampignController.php
+++ web/application/controllers/api/CampignController.php
@@ -49,7 +49,30 @@ class CampignController extends CI_Controller
     public function detail()
     {
         $campign = $this->db->get_where('campaign', ['kode' => $this->input->get('kode')]);
-        $methode = $this->tripay->initChannelPembayaran()->getData();
+        // $methode = $this->tripay->initChannelPembayaran()->getData();
+
+
+        $apiKey = 'xvYjCuHif6GP6J7OTUqrQXgs4xd7kTfMTPXippgO';
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_FRESH_CONNECT  => true,
+            CURLOPT_URL            => 'https://tripay.co.id/api/merchant/payment-channel',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_HEADER         => false,
+            CURLOPT_HTTPHEADER     => ['Authorization: Bearer ' . $apiKey],
+            CURLOPT_FAILONERROR    => false,
+            CURLOPT_IPRESOLVE      => CURL_IPRESOLVE_V4
+        ));
+
+        $methode = curl_exec($curl);
+        // $error = curl_error($curl);
+
+        // curl_close($curl);
+
+        // echo empty($error) ? $response : $error;
+
 
         if ($campign) {
             header('Content-Type: application/json');
