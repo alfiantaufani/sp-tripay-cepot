159cd4257ecb85aec664ec8208ca3324c6dd5eb0
alfiantaufani <alfiantaufani@gmail.com>
2022-09-20 11:31:10 +0700

    tambah i

--- web/application/controllers/api/RiwayatController.php
+++ web/application/controllers/api/RiwayatController.php
@@ -32,17 +32,38 @@ class RiwayatController extends CI_Controller
 
     public function detail()
     {
+        // select DB
         $id = $this->input->get('id');
         $this->db->select('*');
         $this->db->from('transaksi');
         $this->db->join('detail_transaksi', 'transaksi.id=detail_transaksi.id_pembayaran');
         $this->db->where('transaksi.uuid', $id);
         $data = $this->db->get();
+        $hasil = $data->row();
+
+        // cari tutorial di tripay
+        $apiKey = 'xvYjCuHif6GP6J7OTUqrQXgs4xd7kTfMTPXippgO';
+        $payload = ['code' => $hasil->payment_name];
+        $curl = curl_init();
+        curl_setopt_array($curl, [
+            CURLOPT_FRESH_CONNECT  => true,
+            CURLOPT_URL            => 'https://tripay.co.id/api/payment/instruction?' . http_build_query($payload),
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_HEADER         => false,
+            CURLOPT_HTTPHEADER     => ['Authorization: Bearer ' . $apiKey],
+            CURLOPT_FAILONERROR    => false,
+            CURLOPT_IPRESOLVE      => CURL_IPRESOLVE_V4
+        ]);
+        $response = curl_exec($curl);
+        // $error = curl_error($curl);
+        // curl_close($curl);
+        // echo empty($error) ? $response : $error;
 
         header('Content-Type: application/json');
         echo json_encode([
             'status' => 'success',
-            'data' => $data->row(),
+            'data' => $hasil,
+            'tutorial' => $response,
         ]);
     }
 }
